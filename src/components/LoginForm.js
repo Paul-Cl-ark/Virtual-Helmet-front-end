744c0365b5503ce37d0c809702cbad2767792379
Paul-Cl-ark <clark.paul123@gmail.com>
2019-01-31 10:38:11 +0000

    adds semantic-ui and semantic-ui log-in form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,55 @@
 import React, { Component, Fragment } from 'react'
+import { Button, Input, Form, Header, Icon } from 'semantic-ui-react'
 
 class LoginForm extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {}
+	state = {
+		email: '',
+		password: ''
 	}
+
+	handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+	handleSubmit = e => {
+		e.preventDefault()
+		this.setState({ email: '', password: '' })
+	}
+
 	render() {
-		return 'Login'
+		const { email, password } = this.state
+
+		return (
+			<Fragment>
+				<Header as="h2">
+					<Icon name="sign in" />
+					<Header.Content>Log In</Header.Content>
+				</Header>
+				<Form onSubmit={this.handleSubmit}>
+					<Form.Field>
+						<label>Email</label>
+						<Input
+							fluid
+							required
+							placeholder="Email"
+							name="email"
+							value={email}
+							onChange={this.handleChange}
+						/>
+					</Form.Field>
+					<Form.Field>
+						<label>Password</label>
+						<Input
+							fluid
+							required
+							placeholder="Password"
+							name="password"
+							value={password}
+							onChange={this.handleChange}
+						/>
+					</Form.Field>
+					<Button type="submit">Submit</Button>
+				</Form>
+			</Fragment>
+		)
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux'
 import configureStore from './store'
 
 import './index.css'
+import 'semantic-ui-css/semantic.min.css'
+
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker'
 
