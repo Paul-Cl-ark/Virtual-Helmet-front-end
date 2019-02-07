74fda40da32701cb263af7eea0cef2e185d29b95
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-07 20:38:05 +0000

    refactors to use mapbox, still some functionality missing

--- src/containers/MapContainer.js
+++ src/containers/MapContainer.js
@@ -1,30 +1,42 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
-import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react'
 import * as actions from '../actions'
+import MapGL, { Marker, Popup, NavigationControl } from 'react-map-gl'
+import SpotMarker from '../components/SpotMarker'
+
 import { Card, Icon, Image } from 'semantic-ui-react'
+
+const TOKEN = process.env.REACT_APP_MAPBOX_API_KEY
+
+const navStyle = {
+	position: 'absolute',
+	top: 0,
+	left: 0,
+	padding: '10px'
+}
 
 export class MapContainer extends Component {
 	state = {
 		showingInfoWindow: false,
 		activeMarker: {},
-		selectedPlace: {},
-		renderNewMarker: false
+		selectedSpot: null,
+		renderNewMarker: false,
+		viewport: {
+			latitude: 51.520419,
+			longitude: -0.0887159,
+			zoom: 16,
+			bearing: 0,
+			pitch: 0
+		}
 	}
 
-	onMarkerClick = (props, marker, event) => {
+	onMarkerClick = spot => {
 		this.setState({
-			selectedPlace: props,
-			activeMarker: marker,
+			selectedSpot: spot,
+			activeMarker: spot,
 			showingInfoWindow: true,
-			renderNewMarker: false,
-			spotDescription: props.spotDescription,
-			spotImage: props.spotImage,
-			spotRating: props.spotRating,
-			spotRaters: props.spotRaters,
-			spotPosition: this.props.position
+			renderNewMarker: false
 		})
-		console.log(props)
 		this.props.removeSpotForm()
 	}
 
@@ -37,30 +49,23 @@ export class MapContainer extends Component {
 		}
 	}
 
-	onMapClick = (location, map) => {
+	onMapClick = event => {
 		if (!!localStorage.getItem('user')) {
 			this.setState({
 				showingInfoWindow: false,
 				renderNewMarker: true
 			})
 			this.props.renderSpotForm()
-			this.props.addNewSpotLatLng({ lat: location.lat(), lng: location.lng() })
+			this.props.addNewSpotLatLng({ lat: event.lngLat[1], lng: event.lngLat[0] })
 		}
 	}
 
 	renderMarkers = () => {
 		return this.props.spotsReducer.spots.length !== 0
 			? this.props.spotsReducer.spots.map(spot => (
-					<Marker
-						spotDescription={spot.description}
-						position={{ lat: spot.latitude, lng: spot.longitude }}
-						onClick={this.onMarkerClick}
-						spotImage={spot.image}
-						spotRating={spot.rating}
-						spotRaters={spot.raters}
-						key={spot.id}
-						spotId={spot.id}
-					/>
+					<Marker longitude={spot.longitude} latitude={spot.latitude} key={spot.id}>
+						<SpotMarker size={20} onClick={() => this.onMarkerClick(spot)} />
+					</Marker>
 			  ))
 			: null
 	}
@@ -68,62 +73,93 @@ export class MapContainer extends Component {
 	renderNewMarker = () => {
 		const lat = this.props.spotsReducer.selectedLat
 		const lng = this.props.spotsReducer.selectedLng
-		return this.state.renderNewMarker ? <Marker position={{ lat: lat, lng: lng }} /> : null
+		return this.state.renderNewMarker ? (
+			<Marker key="new" latitude={lat} longitude={lng}>
+				<SpotMarker size={20} />
+			</Marker>
+		) : null
 	}
 
 	componentDidMount() {
+		this.locateUser()
 		this.props.getAllSpots()
 	}
 
-	render() {
-		if (!this.props.google) {
-			return <div>Loading...</div>
-		}
+	// new mapbox functions
 
+	updateViewport = viewport => {
+		this.setState({ viewport })
+	}
+
+	locateUser() {
+		navigator.geolocation.getCurrentPosition(position => {
+			this.updateViewport({
+				longitude: position.coords.longitude,
+				latitude: position.coords.latitude,
+				zoom: 16
+			})
+		})
+	}
+
+	renderPopup() {
+		const { selectedSpot } = this.state
+		return selectedSpot ? (
+			<Popup
+				tipSize={5}
+				anchor="top"
+				longitude={selectedSpot.longitude}
+				latitude={selectedSpot.latitude}
+				closeOnClick={false}
+				onClose={() =>
+					this.setState({
+						popupInfo: null
+					})
+				}>
+				<Card>
+					<Image src={this.state.selectedSpot.image} />
+					<Card.Content>
+						<Card.Header>{this.state.selectedSpot.description}</Card.Header>
+						<Card.Meta>Date added</Card.Meta>
+						<Card.Description>
+							{this.state.selectedSpot.latitude} - {this.state.selectedSpot.longitude}
+						</Card.Description>
+					</Card.Content>
+					<Card.Content extra>
+						<a>
+							<Icon name="thumbs up outline" />
+							{this.state.spotRating}
+						</a>
+					</Card.Content>
+				</Card>
+			</Popup>
+		) : null
+	}
+
+	render() {
 		return (
-			<Map
-				google={this.props.google}
-				centerAroundCurrentLocation
-				zoom={16}
-				onClick={(t, map, c) => this.onMapClick(c.latLng, map)}>
+			<MapGL
+				{...this.state.viewport}
+				width="100vw"
+				height="100vh"
+				onViewportChange={this.updateViewport}
+				mapboxApiAccessToken={TOKEN}
+				onClick={event => this.onMapClick(event)}>
 				{this.renderMarkers()}
+				{this.render}
 				{this.renderNewMarker()}
-				<InfoWindow
-					marker={this.state.activeMarker}
-					visible={this.state.showingInfoWindow}
-					onClose={this.onClose}>
-					<Card>
-						<Image
-							src={
-								this.state.image ||
-								'https://www.wibc.com/sites/g/files/exi441/f/styles/large_730/public/article-images-featured/554676-391326.jpg?itok=VPAnqxDA'
-							}
-						/>
-						<Card.Content>
-							<Card.Header>{this.state.spotDescription}</Card.Header>
-							<Card.Meta>Date added</Card.Meta>
-							<Card.Description>{this.state.spotPosition}</Card.Description>
-						</Card.Content>
-						<Card.Content extra>
-							<a>
-								<Icon name="thumbs up outline" />
-								{this.state.spotRating}
-							</a>
-						</Card.Content>
-					</Card>
-				</InfoWindow>
-			</Map>
+				{this.renderPopup()}
+
+				<div className="nav" style={navStyle}>
+					<NavigationControl onViewportChange={this.updateViewport} />
+				</div>
+			</MapGL>
 		)
 	}
 }
 
 const mapStateToProps = state => state
 
-const WrappedMapContainer = GoogleApiWrapper({
-	apiKey: process.env.REACT_APP_GOOGLE_API_KEY
-})(MapContainer)
-
 export default connect(
 	mapStateToProps,
 	actions
-)(WrappedMapContainer)
+)(MapContainer)
