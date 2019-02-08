9e8bdd42eae82c109e6a0b902151f22b2fc2865e
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-08 11:19:31 +0000

    clean up, adds redux dev tools

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import configureStore from './store'
-
-import './index.css'
 import 'semantic-ui-css/semantic.min.css'
 
 import App from './containers/App'
