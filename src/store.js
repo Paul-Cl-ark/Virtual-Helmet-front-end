df562eff5235fe1f50b6e0baf3797f705fc3cb93
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-09 12:50:05 +0000

    persists state to local storage

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/'
+import persistState from 'redux-localstorage'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export default function configureStore() {
-	return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+	return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk), persistState()))
 }
