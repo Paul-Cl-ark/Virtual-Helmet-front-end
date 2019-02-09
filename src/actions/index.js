b5d173f29aa969fbf26f5c8853b654e0a953ff0a
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-09 15:26:44 +0000

    handles log in and registration ui display

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,8 @@ export const registerUser = user => {
 		API.registerUser(user).then(data => {
 			history.push('/')
 			dispatch({ type: 'REGISTER_USER', payload: data })
+			dispatch({ type: 'REMOVE_OR_BUTTON' })
+			dispatch({ type: 'RENDER_NAVBAR' })
 		})
 	}
 }
@@ -15,6 +17,8 @@ export const authenticateUser = user => {
 		return API.authenticateUser(user).then(data => {
 			history.push('/')
 			dispatch({ type: 'AUTHENTICATE_USER', payload: data })
+			dispatch({ type: 'REMOVE_OR_BUTTON' })
+			dispatch({ type: 'RENDER_NAVBAR' })
 		})
 	}
 }
@@ -23,8 +27,12 @@ export const logOutUser = () => {
 	return dispatch => {
 		return API.logOutUser().then(data => {
 			history.push('/')
-			dispatch({ type: 'TOGGLE_MENU' })
 			dispatch({ type: 'LOGOUT_USER' })
+			dispatch({ type: 'CLOSE_MENU' })
+			dispatch({ type: 'REMOVE_SPOT_FORM' })
+			dispatch({ type: 'REMOVE_NAVBAR' })
+			dispatch({ type: 'REMOVE_POP_UP' })
+			dispatch({ type: 'RENDER_OR_BUTTON' })
 		})
 	}
 }
