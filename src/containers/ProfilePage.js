db0bbd376db087daa3d3a2ab123337284c38eb0d
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-10 13:01:05 +0000

    adds header with user name and join date

--- src/containers/ProfilePage.js
+++ src/containers/ProfilePage.js
@@ -1,13 +1,24 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Container, Header, Image } from 'semantic-ui-react'
 
 class ProfilePage extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {}
-	}
 	render() {
-		return <Fragment>ProfilePage - user will be able to edit their information</Fragment>
+		const { firstName, lastName, date } = this.props.user
+		return (
+			<Container>
+				<Header as="h2">
+					<Image circular src="https://react.semantic-ui.com/images/avatar/large/patrick.png" />{' '}
+					{firstName}
+				</Header>
+				<p>Member since: {date}</p>
+			</Container>
+		)
 	}
 }
 
-export default ProfilePage
+const mapStateToProps = state => ({
+	user: state.users.user
+})
+
+export default connect(mapStateToProps)(ProfilePage)
