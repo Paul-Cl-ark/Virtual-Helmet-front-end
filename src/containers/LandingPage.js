618e753afbd74869a5bbd2e700283a4ae1ae44f8
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-10 13:50:19 +0000

    fixes bug with buttons

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -2,24 +2,28 @@ import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import Map from '../components/Map'
 import NewSpotForm from '../components/NewSpotForm'
+import OrButton from '../components/OrButton'
 
 class LandingPage extends Component {
 	renderNewSpotForm = () => {
 		return this.props.renderSpotForm ? <NewSpotForm /> : null
 	}
+	renderOrButton = () => (this.props.renderOrButton ? <OrButton /> : null)
 
 	render() {
 		return (
 			<Fragment>
 				<Map />
 				{this.renderNewSpotForm()}
+				{this.renderOrButton()}
 			</Fragment>
 		)
 	}
 }
 
 const mapStateToProps = state => ({
-	renderSpotForm: state.appActions.renderSpotForm
+	renderSpotForm: state.appActions.renderSpotForm,
+	renderOrButton: state.appActions.renderOrButton
 })
 
 export default connect(mapStateToProps)(LandingPage)
