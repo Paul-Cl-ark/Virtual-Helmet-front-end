2fd9ec38a38602ca1b2f99ad7e61fd011eb157f4
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-12 23:32:21 +0000

    tiny refactor

--- src/components/SpotPopUp.js
+++ src/components/SpotPopUp.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import MapGL, { Popup } from 'react-map-gl'
+import { Popup } from 'react-map-gl'
 import SpotPopUpCard from './SpotPopUpCard'
 import { deSelectSpot, removePopUp } from '../actions'
 
