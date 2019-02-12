7e07e5ca191c5bc9d53d5877bddeb5842d3ffb3e
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-12 21:27:47 +0000

    quick setup, needs refactoring

--- src/containers/SpotPage.js
+++ src/containers/SpotPage.js
@@ -3,18 +3,28 @@ import { Image, Container, Header } from 'semantic-ui-react'
 // import { connect } from 'react-redux'
 
 class SpotPage extends Component {
-	state = { spot: null }
 	componentDidMount() {
 		const { id } = this.props.match.params
 		fetch(`/API/spots/${id}`)
 			.then(response => response.json())
 			.then(data => {
-				this.setState({ spot: data })
+				const { image, latitude, longitude, description } = data
+				this.setState({ image, latitude, longitude, description })
 			})
 	}
 
 	renderImage = () => {
-		return this.state.spot ? this.state.spot.image : null
+		return this.state ? this.state.image : null
+	}
+	renderLatitude = () => {
+		return this.state ? this.state.latitude : null
+	}
+	renderLongitude = () => {
+		return this.state ? this.state.longitude : null
+	}
+
+	renderDescription = () => {
+		return this.state ? this.state.description : null
 	}
 
 	render() {
@@ -22,6 +32,10 @@ class SpotPage extends Component {
 			<Fragment>
 				<Container>
 					<Image src={this.renderImage()} />
+					<p>{this.renderDescription()}</p>
+					<p>
+						Latitude: {this.renderLatitude()}, Longitude: {this.renderLongitude()}
+					</p>
 				</Container>
 			</Fragment>
 		)
