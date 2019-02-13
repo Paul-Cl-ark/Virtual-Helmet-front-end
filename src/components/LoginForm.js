7ccffa512ab9e7182ce6c9cac6db64dffd30f766
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-13 20:09:44 +0000

    bsic styling and logo added

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,14 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { authenticateUser } from '../actions'
+import TextLogo from '../components/TextLogo'
 
-import { Button, Input, Form, Header, Icon } from 'semantic-ui-react'
+import { Button, Input, Form, Header, Icon, Grid, Container } from 'semantic-ui-react'
+
+const containerStyle = {
+	justifyContent: 'center',
+	margin: '20px'
+}
 
 class LoginForm extends Component {
 	state = {
@@ -22,37 +28,44 @@ class LoginForm extends Component {
 
 		return (
 			<Fragment>
-				<Header as="h2">
-					<Icon name="sign in" />
-					<Header.Content>Log In</Header.Content>
-				</Header>
-				<Form onSubmit={this.handleSubmit}>
-					<Form.Field>
-						<label>Email</label>
-						<Input
-							fluid
-							required
-							type="email"
-							placeholder="Email"
-							name="email"
-							value={email}
-							onChange={this.handleChange}
-						/>
-					</Form.Field>
-					<Form.Field>
-						<label>Password</label>
-						<Input
-							fluid
-							required
-							type="password"
-							placeholder="Password"
-							name="password"
-							value={password}
-							onChange={this.handleChange}
-						/>
-					</Form.Field>
-					<Button type="submit">Log In</Button>
-				</Form>
+				<TextLogo />
+				<Container style={containerStyle}>
+					<Header as="h2">
+						<Icon name="sign in" />
+						<Header.Content>Log In</Header.Content>
+					</Header>
+					<Form onSubmit={this.handleSubmit}>
+						<Form.Field>
+							<label>Email</label>
+							<Input
+								fluid
+								required
+								type="email"
+								placeholder="Email"
+								name="email"
+								value={email}
+								onChange={this.handleChange}
+							/>
+						</Form.Field>
+						<Form.Field>
+							<label>Password</label>
+							<Input
+								fluid
+								required
+								type="password"
+								placeholder="Password"
+								name="password"
+								value={password}
+								onChange={this.handleChange}
+							/>
+						</Form.Field>
+						<Grid>
+							<Grid.Column textAlign="center">
+								<Button type="submit">Log In</Button>
+							</Grid.Column>
+						</Grid>
+					</Form>
+				</Container>
 			</Fragment>
 		)
 	}
