d3b7f1223999aa1dfd8430140f1d25fe00867486
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-13 00:19:27 +0000

    updates 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
 
+const imgStyle = {
+	height: '100vh',
+	width: 'auto'
+}
+
 class NotFound extends Component {
 	render() {
-		return '404 not found'
+		return (
+			<img style={imgStyle} src="/images/404.jpeg" alt="404 not found error, please press back" />
+		)
 	}
 }
 
