d15694224b7f8053e44a09de766feea7f1358328
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-13 19:26:18 +0000

    prevents a user voting on their own spots

--- src/components/RatingButtons.js
+++ src/components/RatingButtons.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { toast } from 'react-toastify'
 import { rateSpot } from '../actions'
 import { Card, Icon } from 'semantic-ui-react'
 
 class RatingButtons extends Component {
+	handleRating = value => {
+		const { user, _id } = this.props.spot
+		if (this.props.userId !== user) {
+			return this.props.rateSpot(_id, value)
+		}
+		toast.warn("You can't vote on your own spot!")
+	}
+
 	render() {
-		const spot = this.props.selectedSpot
-		const { rating, _id } = spot
+		const { rating } = this.props.spot
+
 		return (
 			<Card.Content extra>
 				Rating : {rating}
-				<a onClick={() => this.props.rateSpot(_id, 1)}>
+				<a onClick={() => this.handleRating(1)}>
 					<Icon name="thumbs up outline" />
 				</a>
-				<a onClick={() => this.props.rateSpot(_id, -1)}>
+				<a onClick={() => this.handleRating(-1)}>
 					<Icon name="thumbs down outline" />
 				</a>
 			</Card.Content>
@@ -22,7 +31,8 @@ class RatingButtons extends Component {
 }
 
 const mapStateToProps = state => ({
-	selectedSpot: state.spots.selectedSpot
+	spot: state.spots.selectedSpot,
+	userId: state.users.user._id
 })
 
 export default connect(
