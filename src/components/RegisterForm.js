53416b088b0a8d6b48c307387506b049d2e5bd0d
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-13 20:05:57 +0000

    centers button

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,8 +1,13 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { registerUser } from '../actions'
+import TextLogo from '../components/TextLogo'
+import { Button, Input, Form, Header, Icon, Container, Grid } from 'semantic-ui-react'
 
-import { Button, Input, Form, Header, Icon } from 'semantic-ui-react'
+const containerStyle = {
+	justifyContent: 'center',
+	margin: '20px'
+}
 
 class RegisterForm extends Component {
 	state = {
@@ -30,71 +35,78 @@ class RegisterForm extends Component {
 
 		return (
 			<Fragment>
-				<Header as="h2">
-					<Icon name="sign in" />
-					<Header.Content>Register</Header.Content>
-				</Header>
-				<Form onSubmit={this.handleSubmit}>
-					<Form.Field>
-						<label>First Name</label>
-						<Input
-							fluid
-							required
-							placeholder="First Name"
-							name="firstName"
-							value={firstName}
-							onChange={this.handleChange}
-						/>
-					</Form.Field>
-					<Form.Field>
-						<label>Last Name</label>
-						<Input
-							fluid
-							required
-							placeholder="Last Name"
-							name="lastName"
-							value={lastName}
-							onChange={this.handleChange}
-						/>
-					</Form.Field>
-					<Form.Field>
-						<label>Email</label>
-						<Input
-							fluid
-							required
-							type="email"
-							placeholder="Email"
-							name="email"
-							value={email}
-							onChange={this.handleChange}
-						/>
-					</Form.Field>
-					<Form.Field>
-						<label>Password</label>
-						<Input
-							fluid
-							required
-							type="password"
-							placeholder="Password"
-							name="password"
-							value={password}
-							onChange={this.handleChange}
-						/>
-					</Form.Field>
-					<Form.Field>
-						<label>Confirm Password</label>
-						<Input
-							fluid
-							required
-							type="password"
-							placeholder="Confirm Password"
-							name="passwordConfirmation"
-							value={passwordConfirmation}
-							onChange={this.handleChange}
-						/>
-					</Form.Field>
-					<Button type="submit">Register</Button>
-				</Form>
+				<TextLogo />
+				<Container style={containerStyle}>
+					<Header as="h2">
+						<Icon name="sign in" />
+						<Header.Content>Register</Header.Content>
+					</Header>
+					<Form onSubmit={this.handleSubmit}>
+						<Form.Field>
+							<label>First Name</label>
+							<Input
+								fluid
+								required
+								placeholder="First Name"
+								name="firstName"
+								value={firstName}
+								onChange={this.handleChange}
+							/>
+						</Form.Field>
+						<Form.Field>
+							<label>Last Name</label>
+							<Input
+								fluid
+								required
+								placeholder="Last Name"
+								name="lastName"
+								value={lastName}
+								onChange={this.handleChange}
+							/>
+						</Form.Field>
+						<Form.Field>
+							<label>Email</label>
+							<Input
+								fluid
+								required
+								type="email"
+								placeholder="Email"
+								name="email"
+								value={email}
+								onChange={this.handleChange}
+							/>
+						</Form.Field>
+						<Form.Field>
+							<label>Password</label>
+							<Input
+								fluid
+								required
+								type="password"
+								placeholder="Password"
+								name="password"
+								value={password}
+								onChange={this.handleChange}
+							/>
+						</Form.Field>
+						<Form.Field>
+							<label>Confirm Password</label>
+							<Input
+								fluid
+								required
+								type="password"
+								placeholder="Confirm Password"
+								name="passwordConfirmation"
+								value={passwordConfirmation}
+								onChange={this.handleChange}
+							/>
+						</Form.Field>
+						<Grid>
+							<Grid.Column textAlign="center">
+								<Button type="submit">Register</Button>
+							</Grid.Column>
+						</Grid>
+					</Form>
+				</Container>
 			</Fragment>
 		)
 	}
