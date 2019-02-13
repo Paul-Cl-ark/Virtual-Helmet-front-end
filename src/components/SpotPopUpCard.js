b56647e0635fc880f4b92c9bd42ce71e275693ed
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-13 19:46:36 +0000

    style card and adds image for type

--- src/components/SpotPopUpCard.js
+++ src/components/SpotPopUpCard.js
@@ -20,22 +20,25 @@ class SpotPopUpCard extends Component {
 	render() {
 		const spot = this.props.selectedSpot
 		const { _id, type, date, description, latitude, longitude } = spot
-		const momentDate = moment(date)
-			.endOf('day')
-			.fromNow()
+		const momentDate = moment(date).format('LL')
 		const imageContent = this.props.user ? (
 			<CardImage />
 		) : (
 			<Card.Meta>'Please log in to see more' </Card.Meta>
 		)
-
+		const capitalizedType = type.charAt(0).toUpperCase() + type.slice(1)
 		return (
 			<Card key={_id} style={popUpStyle}>
 				{imageContent}
 				<Card.Content>
 					<Card.Header>{description}</Card.Header>
 					<Card.Meta>
-						Date added:{momentDate}, type: {type}
+						<p>Date added: {momentDate}</p>
+						<p>
+							Type:
+							<img style={{ height: '20px', width: '20px' }} src={`/images/${type}-icon.svg`} />
+							{capitalizedType}
+						</p>
 					</Card.Meta>
 					<Card.Description>
 						Latitude: {latitude}, Longitude: {longitude}
